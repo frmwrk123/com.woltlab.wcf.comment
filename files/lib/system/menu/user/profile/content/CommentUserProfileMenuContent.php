a99718795310ca20ec4ddd44d9004a90c3a0eb1e
Alexander Ebert <ebert@woltlab.com>
2011-11-09 02:01:32 +0100

    Added user profile content manager

---
+++ files/lib/system/menu/user/profile/content/CommentUserProfileMenuContent.php
@@ -0,0 +1,21 @@
+<?php
+namespace wcf\system\menu\user\profile\content;
+
+/**
+ * Handles user profile comment content.
+ * 
+ * @author	Alexander Ebert
+ * @copyright	2001-2011 WoltLab GmbH
+ * @license	GNU Lesser General Public License <http://opensource.org/licenses/lgpl-license.php>
+ * @package	com.woltlab.wcf.comment
+ * @subpackage	system.menu.user.profile.content
+ * @category 	Community Framework
+ */
+class CommentUserProfileMenuContent implements IUserProfileMenuContent {
+	/**
+	 * @see	wcf\system\menu\user\profile\content\IUserProfileMenuContent::getContent()
+	 */
+	public function getContent() {
+		die('IMPLEMENT ME!');
+	}
+}
