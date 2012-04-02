b2cf8f1a6668a6d415439ea02f16c8913fc7c1bb
Alexander Ebert <ebert@woltlab.com>
2012-04-02 21:46:25 +0200

    Added user notification events

---
+++ files/lib/system/user/notification/object/type/UserProfileCommentUserNotificationObjectType.class.php
@@ -0,0 +1,55 @@
+<?php
+namespace wcf\system\user\notification\object\type;
+use wcf\data\comment\Comment;
+use wcf\data\comment\CommentList;
+use wcf\data\object\type\AbstractObjectTypeProcessor;
+use wcf\system\user\notification\object\type\AbstractUserNotificationObjectType;
+use wcf\system\user\notification\object\CommentUserNotificationObject;
+
+/**
+ * Represents a comment notification object type.
+ *
+ * @author	Alexander Ebert
+ * @copyright	2001-2012 WoltLab GmbH
+ * @license	GNU Lesser General Public License <http://opensource.org/licenses/lgpl-license.php>
+ * @package	com.woltlab.wcf.user
+ * @subpackage	system.user.notification.object.type
+ * @category 	Community Framework
+ */
+class UserProfileCommentNotificationObjectType extends AbstractObjectTypeProcessor implements IUserNotificationObjectType {
+	/**
+	 * @see wcf\system\user\notification\object\type\IUserNotificationObjectType::getObjectByID()
+	 */
+	public function getObjectByID($objectID) {
+		$object = new Comment($objectID);
+		if (!$object->commentID) {
+			// create empty object for unknown request id
+			$object = new Comment(null, array('commentID' => $objectID));
+		}
+		
+		return array($object->commentID => new CommentUserNotificationObject($object));
+	}
+
+	/**
+	 * @see wcf\system\user\notification\object\type\IUserNotificationObjectType::getObjectsByIDs()
+	 */
+	public function getObjectsByIDs(array $objectIDs) {
+		$objectList = new CommentList();
+		$objectList->getConditionBuilder()->add("comment.commentID IN (?)", array($objectIDs));
+		$objectList->readObjects();
+		
+		$objects = array();
+		foreach ($objectList as $object) {
+			$objects[$object->commentID] = new CommentUserNotificationObject($object);
+		}
+		
+		foreach ($objectIDs as $objectID) {
+			// append empty objects for unknown ids
+			if (!isset($objects[$objectID])) {
+				$objects[$objectID] = new CommentUserNotificationObject(new Comment(null, array('commentID' => $objectID)));
+			}
+		}
+		
+		return $objects;
+	}
+}
