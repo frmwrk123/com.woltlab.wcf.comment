f3468f1bde637065f03cb0d256aa60fae1c59f71
Tim Düsterhus <duesterhus@woltlab.com>
2013-01-07 17:45:47 +0100

    Order uses in ViewableComment und ViewableCommentResponse

--- files/lib/data/comment/response/ViewableCommentResponse.class.php
+++ files/lib/data/comment/response/ViewableCommentResponse.class.php
@@ -1,8 +1,8 @@
 <?php
 namespace wcf\data\comment\response;
-use wcf\data\DatabaseObjectDecorator;
 use wcf\data\user\User;
 use wcf\data\user\UserProfile;
+use wcf\data\DatabaseObjectDecorator;
 
 /**
  * Represents a viewable comment response.
